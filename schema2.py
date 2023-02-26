3748208623f83e55b1d7d832adde0374af144724
Aprotim Biswas <biswasaprotim2011@outlook.com>
2023-02-26 12:15:08 +0530

    Python and Strawberry

---
+++ schema2.py
@@ -0,0 +1,23 @@
+from typing import Optional
+import strawberry
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self, name: Optional[str] = None) -> str:
+        if name is None:
+            return "Hello World"
+        return f"Hello {name}"
+    
+    @strawberry.field
+    def greet(self, name: Optional[str] = strawberry.UNSET) -> str:
+        if name is strawberry.UNSET:
+            return "Name was not set!"
+        if name is None:
+            return "Name was null!"
+        return f"Hello {name}"
+    
+
+schema = strawberry.Schema(query=Query)
+print(schema)
